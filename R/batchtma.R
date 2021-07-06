5a4f3bc3c085011a136153d4b7f10b4926ee1d78
stopsack <stopsack@mskcc.org>
2021-07-06 19:25:10 +0200

    fix nonprinting character in .Rds

--- R/batchtma.R
+++ R/batchtma.R
@@ -19,8 +19,8 @@
 #' @seealso \url{https://stopsack.github.io/batchtma}
 #' @references
 #' Stopsack KH, Tyekucheva S, Wang M, Gerke TA, Vaselkiv JB, Penney KL,
-#' Kantoff PW, Finn SP, Fiorentino M, Loda M, Lotan TL, Parmigiani G\*,
-#' Mucci LA\* (\* equal contribution). Extent, impact, and mitigation of
+#' Kantoff PW, Finn SP, Fiorentino M, Loda M, Lotan TL, Parmigiani G+,
+#' Mucci LA+ (+ equal contribution). Extent, impact, and mitigation of
 #' batch effects in tumor biomarker studies using tissue microarrays.
 #' bioRxiv 2021.06.29.450369; doi: https://doi.org/10.1101/2021.06.29.450369
 NULL
