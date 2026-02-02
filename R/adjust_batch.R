4c3cda0192e4550d91eef1fad3d0c6a1f10dec3d
Konrad H. Stopsack <35557324+stopsack@users.noreply.github.com>
2026-02-02 20:50:27 +0100

    dots should no longer appear in pipes

--- R/adjust_batch.R
+++ R/adjust_batch.R
@@ -1,9 +1,3 @@
-# as per https://github.com/jennybc/googlesheets/blob/master/R/googlesheets.R:
-## quiets concerns of R CMD check re: the .'s that appear in pipelines
-if (getRversion() >= "2.15.1") {
-  utils::globalVariables(c("."))
-}
-
 #' Adjust for batch effects
 #'
 #' @description
